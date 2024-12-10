05e94b37ce185a5d30c9d6aadd2def47c0c32e01
Łukasz Strzelczyk <lstrzelczyk.business@gmail.com>
2024-12-11 00:05:30 +0100

    part 2 of day 3 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,51 @@
 use regex::Regex;
-use std::{
-    fs::File,
-    io::{BufReader, Read},
-    process::exit,
-};
+use std::{fs::File, io::Read, process::exit};
+
+#[allow(dead_code)]
+fn part1(buffer: String) -> i32 {
+    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    let commands: Vec<_> = re.find_iter(&buffer).map(|m| m.as_str()).collect();
+    let comm_string: Vec<String> = commands.iter().map(|&x| x.to_string()).collect();
+    let mut sum: i32 = 0;
+    for mut command in comm_string {
+        command.pop();
+        for _ in 0..4 {
+            command.remove(0);
+        }
+        let (val1, val2) = command.split_once(",").unwrap();
+        //println!("(val1: {val1}, val2: {val2})");
+        let val1: i32 = val1.parse().unwrap();
+        let val2: i32 = val2.parse().unwrap();
+        sum += val1 * val2
+    }
+    sum
+}
+#[allow(dead_code)]
+fn part2(buffer: String) -> i32 {
+    let mut sum = 0;
+    let mut enable = true; //enables reading mul command
+    let re = Regex::new(r"(?P<mul>mul\((?P<val1>[0-9]{1,3}),(?P<val2>[0-9]{1,3})\))|(?P<do>do\(\))|(?P<dn>don't\(\))"
+    )
+        .unwrap();
+    for capture in re.captures_iter(&buffer) {
+        if capture.name("do").is_some() {
+            enable = true;
+        }
+        if capture.name("dn").is_some() {
+            //println!("dn");
+            enable = false;
+        }
+        if capture.name("mul").is_some() {
+            //println!("enable: {enable}");
+            if enable {
+                let val1: i32 = capture["val1"].parse().unwrap();
+                let val2: i32 = capture["val2"].parse().unwrap();
+                sum += val1 * val2;
+            }
+        }
+    }
+    sum
+}
 
 fn main() {
     let path = "input.txt";
@@ -19,24 +61,7 @@ fn main() {
         println!("Error while reading a file: {path}, error: {e:?}");
         exit(2);
     }
-    let re =Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
-    let commands:Vec<_>=re.find_iter(&buffer).map(|m| m.as_str()).collect();
-    let comm_string:Vec<String>=commands.iter().map(|&x| {
-       x.to_string()
-    }).collect();
-    let mut sum:i32=0;
-    for mut command in comm_string{
-        command.pop();
-        for _ in 0..4{
-            command.remove(0);
-        }
-        let (val1,val2)=command.split_once(",").unwrap();
-        //println!("(val1: {val1}, val2: {val2})");
-        let val1:i32=val1.parse().unwrap();
-        let val2:i32=val2.parse().unwrap();
-        sum+=val1*val2
-    };
-    println!("sum: {sum}")
-    
 
+    //println!("sum: {}",part1(buffer))
+    println!("sum: {}", part2(buffer))
 }
